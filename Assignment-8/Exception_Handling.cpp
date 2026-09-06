378be2f5228b3bf1c40569937092707fb322c443
Hardik-K12 <hardikkapratwar2006@gmail.com>
2026-09-06 22:16:14 +0530

    Create Exception_Handling.cpp

---
+++ Assignment-8/Exception_Handling.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    try
+    {
+        // Check for division by zero
+        if(b == 0)
+            throw b;
+
+        cout << "Result = " << a / b;
+    }
+    catch(int)
+    {
+        cout << "Exception: Cannot divide by zero";
+    }
+
+    return 0;
+}
