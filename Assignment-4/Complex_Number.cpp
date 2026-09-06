d8424da8954a2a2195a1d9a1598673a08e4bcddb
Hardik-K12 <hardikkapratwar2006@gmail.com>
2026-09-06 21:59:42 +0530

    Update Complex_Number.cpp

--- Assignment-4/Complex_Number.cpp
+++ Assignment-4/Complex_Number.cpp
@@ -1,113 +1,109 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class Complex
 {
-    float real, imag;
+private:
+    int real, img;
 
 public:
 
-    // Take complex number values
-    void accept()
+    // Store the real and imaginary parts
+    void accept(int r, int i)
     {
-        cout<<"Enter real part: ";
-        cin>>real;
-
-        cout<<"Enter imaginary part: ";
-        cin>>imag;
+        real = r;
+        img = i;
     }
 
-    // Display complex number
+    // Display the complex number
     void display()
     {
-        cout<<real;
-
-        if(imag>=0)
-            cout<<" + "<<imag<<"i";
+        if(img > 0)
+            cout << real << " + i" << img;
         else
-            cout<<" - "<<-imag<<"i";
-
-        cout<<endl;
+            cout << real << " - i" << -img;
     }
 
     // Add two complex numbers
-    Complex add(Complex c)
+    Complex add(Complex X)
     {
-        Complex temp;
-        temp.real=real+c.real;
-        temp.imag=imag+c.imag;
-        return temp;
+        Complex sum;
+        sum.real = real + X.real;
+        sum.img = img + X.img;
+        return sum;
     }
 
     // Subtract two complex numbers
-    Complex subtract(Complex c)
+    Complex subtract(Complex X)
     {
-        Complex temp;
-        temp.real=real-c.real;
-        temp.imag=imag-c.imag;
-        return temp;
+        Complex diff;
+        diff.real = real - X.real;
+        diff.img = img - X.img;
+        return diff;
     }
 
     // Multiply two complex numbers
-    Complex multiply(Complex c)
+    Complex multiply(Complex X)
     {
-        Complex temp;
-        temp.real=(real*c.real)-(imag*c.imag);
-        temp.imag=(real*c.imag)+(imag*c.real);
-        return temp;
+        Complex mul;
+        mul.real = (real * X.real) - (img * X.img);
+        mul.img = (real * X.img) + (img * X.real);
+        return mul;
     }
 
     // Divide two complex numbers
-    Complex divide(Complex c)
+    Complex divide(Complex X)
     {
-        Complex temp;
-        float d=(c.real*c.real)+(c.imag*c.imag);
+        Complex div;
+        int den = (X.real * X.real) + (X.img * X.img);
 
-        temp.real=((real*c.real)+(imag*c.imag))/d;
-        temp.imag=((imag*c.real)-(real*c.imag))/d;
+        div.real = ((real * X.real) + (img * X.img)) / den;
+        div.img = ((img * X.real) - (real * X.img)) / den;
 
-        return temp;
+        return div;
     }
 
-    // Find complex conjugate
+    // Find the complex conjugate
     Complex conjugate()
     {
-        Complex temp;
-        temp.real=real;
-        temp.imag=-imag;
-        return temp;
+        Complex con;
+        con.real = real;
+        con.img = -img;
+        return con;
     }
 };
 
 int main()
 {
-    Complex c1,c2,result;
+    Complex C1, C2, C3;
 
-    cout<<"Enter first complex number:\n";
-    c1.accept();
+    cout << "C1 object is created\n";
+    C1.accept(3, 5);
+    C1.display();
 
-    cout<<"\nEnter second complex number:\n";
-    c2.accept();
+    cout << "\n\nC2 object is created\n";
+    C2.accept(7, 5);
+    C2.display();
 
-    result=c1.add(c2);
-    cout<<"\nAddition: ";
-    result.display();
+    cout << "\n\nAddition of Complex Numbers\n";
+    C3 = C1.add(C2);
+    C3.display();
 
-    result=c1.subtract(c2);
-    cout<<"Subtraction: ";
-    result.display();
+    cout << "\n\nSubtraction of Complex Numbers\n";
+    C3 = C1.subtract(C2);
+    C3.display();
 
-    result=c1.multiply(c2);
-    cout<<"Multiplication: ";
-    result.display();
+    cout << "\n\nMultiplication of Complex Numbers\n";
+    C3 = C1.multiply(C2);
+    C3.display();
 
-    result=c1.divide(c2);
-    cout<<"Division: ";
-    result.display();
+    cout << "\n\nDivision of Complex Numbers\n";
+    C3 = C1.divide(C2);
+    C3.display();
 
-    result=c1.conjugate();
-    cout<<"Conjugate of first number: ";
-    result.display();
+    cout << "\n\nConjugate of First Complex Number\n";
+    C3 = C1.conjugate();
+    C3.display();
 
     return 0;
 }
