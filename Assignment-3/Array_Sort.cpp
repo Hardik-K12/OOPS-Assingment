0d152406e9db14cd0690be3e09a5f222b1487f2f
Hardik-K12 <hardikkapratwar2006@gmail.com>
2026-09-06 21:52:15 +0530

    Create Array_Sort.cpp

---
+++ Assignment-3/Array_Sort.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+// Take array size and elements
+void accept(int &a, int c[])
+{
+    cout << "Enter number of array elements: ";
+    cin >> a;
+
+    cout << "Enter array elements: ";
+    for(int i = 0; i < a; i++)
+        cin >> c[i];
+}
+
+// Display array elements
+void display(int m, int arr[])
+{
+    cout << "The array is: ";
+    for(int i = 0; i < m; i++)
+        cout << arr[i] << " ";
+}
+
+// Swap two array elements
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+// Sort array in ascending order
+void sort(int m, int arr[])
+{
+    for(int i = 0; i < m - 1; i++)
+    {
+        for(int j = 0; j < m - i - 1; j++)
+        {
+            if(arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
+        }
+    }
+}
+
+int main()
+{
+    int n, arr[10];
+
+    accept(n, arr);
+
+    cout << "\nThe unsorted array is: ";
+    display(n, arr);
+
+    sort(n, arr);
+
+    cout << "\nThe sorted array is: ";
+    display(n, arr);
+
+    return 0;
+}
